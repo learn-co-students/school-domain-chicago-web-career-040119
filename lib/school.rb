d35f4062cd6d652992dff70bbadbd4c743706eba
Jose Elera <jelera@gmail.com>
2019-04-02 18:02:51 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+
+  def add_student(student, grade)
+    if !@roster.has_key?(grade)
+      @roster[grade] = []
+    end
+
+    @roster[grade] << student
+  end
+
+  def roster
+    @roster
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.transform_values { |student_arr| student_arr.sort }
+
+  end
+
+end
