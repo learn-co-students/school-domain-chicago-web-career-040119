265b839c63b9e5359ed2dab46e434b10e1b98146
jackrobbins1@gmail.com <jackrobbins1@gmal.com>
2019-04-02 17:46:51 -0500

    began working on school.rb

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+require "pry"
+
+class School
+
+  roster = {}
+
+  # def initialize(name)
+  #   @name = name
+  # end
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+    roster << name
+  end
+
+end
+
+school = School.new("Bayside High School")
