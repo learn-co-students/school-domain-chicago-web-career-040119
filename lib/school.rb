98b8c7eaa9b6d63c6b5ba5fe59145a5084b784f5
diautzi <dia_utzi@yahoo.com>
2019-04-03 16:57:48 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School
+attr_accessor :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster={}
+end
+
+
+def add_student(name, grade)
+  if @roster[grade]
+    @roster[grade] << name
+  else
+  @roster[grade] = [name]
+  # @roster[:grade] << name
+end
+end
+
+def grade(student_grade)
+  @roster.map do |k, v|
+    if student_grade == k
+      return v
+    end
+  end
+end
+
+def sort
+sorted_hash = {}
+@roster.sort.each do |grade, name|
+sorted_hash[grade]= name.sort
+end
+sorted_hash
+end
+end
