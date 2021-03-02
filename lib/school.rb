635a7a136a57a11a3d84528a6a21aac8710ca02c
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-03-02 15:33:32 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,36 +1,41 @@
-# code here!
 class School
-attr_accessor :name, :roster
+
+  attr_accessor :name, :roster
 
   def initialize(name)
-    @school=name
+    @name = name
     @roster = {}
-  end
+  end 
 
   def add_student(name, grade)
     if @roster[grade]
+      puts @roster[grade]
       @roster[grade] << name
-    else
+    else 
       @roster[grade] = [name]
-    end
-  end
+    end 
+  end 
 
   def grade(grade)
-    @roster[grade]
-  end
+    if @roster[grade]
+      @roster[grade]
+    else 
+      puts 'this roster does not exist'
+    end 
+  end 
 
   def sort
     sorted_hash = {}
     @roster.map do |grade, name|
-      sorted_hash[grade] = (name.sort)
-  end
-  sorted_hash
- end
+      sorted_hash[grade] = name.sort
+    end 
+    sorted_hash
+  end 
 
 end
 
-school = School.new("Bayside High School")
-school.add_student("Zach Morris", 9)
-school.roster
-
-school.sort
+# school = School.new("Bayside High School")
+# school.add_student("Zach Morris", 9)
+# school.roster
+#
+# school.sort
